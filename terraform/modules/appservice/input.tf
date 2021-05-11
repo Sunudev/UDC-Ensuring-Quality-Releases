d72f36cb4f9c35e143187c4446d9b25c66b61c44
Moussa Seck <sunutek95@gmail.com>
2021-05-10 23:02:10 -0500

    Update 20

--- terraform/modules/appservice/input.tf
+++ terraform/modules/appservice/input.tf
@@ -1,6 +1,21 @@
-# Resource Group/Location
-variable "location" {}
+resource "azurerm_app_service_plan" "test" {
+  name                = "${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
 
-variable "application_type" {}
-variable "resource_type" {}
-variable "resource_group" {}
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "test" {
+  name                = "${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+  app_service_plan_id = azurerm_app_service_plan.test.id
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = 1
+  }
+}
