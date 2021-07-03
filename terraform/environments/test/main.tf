b74acdb1273b580f33d782b792a954961198d577
Moussa Seck <moussa.seck@sunudev.com>
2021-07-03 06:10:48 -0500

    update1

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -7,6 +7,7 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
+    resource_group_name  = "${var.resource_group}"
     storage_account_name = "tstate24177"
     container_name       = "tstate"
     key                  = "terraform.tfstate"
